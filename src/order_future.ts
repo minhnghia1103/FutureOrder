8886f8e855a635d48f02d9de90580f5275767027
Minh Nghĩa <109430898+minhnghia1103@users.noreply.github.com>
2024-08-13 12:08:45 +0700

    third

--- src/order_future.ts
+++ src/order_future.ts
@@ -83,11 +83,6 @@ class OrderFuture {
     
         try {
             const response = await fetch(url, requestOptions);
-    
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-    
             const data = await response.json();
             console.log(data);
         } catch (error) {
@@ -104,7 +99,7 @@ const body_market_open: body_market_open = {
     side: 1, // 1: long_open, 3: short_open, 4: long_close, 2: short_close
     openType: 1, // 1: isolated, 2: cross
     type: 5, // 5: market, 1: limit
-    vol: 2, // 1vol = 5.9497 USDT
+    vol: 1.7, // 1vol = 5.9497 USDT
     leverage: 10, // đòn bẩy
     marketCeiling: false,
     priceProtect: 0
@@ -142,4 +137,4 @@ const body_limit_close: body_limit_close = {
 }
 const cookie = 'kcex_theme_main=light; kcex_exchange_orderbook_col3=quan; g_state={"i_l":0}; kcex_base_fiat=VND; NEXT_LOCALE=vi-VN; kcex_reset_tradingview_key=false; _iidt=PtLergYtK5TKjAIuEbuqIlZstQtU6dHpZOQzHmZP3cln5xPEC1fPlIUjI3LnJvQRMdjguo8cT84drg==; _vid_t=jv9aldEv1dN4Ko/airBNsiH7lbaMHQTv0qQ+024Zh6153u/koTBFLiI9s8GewS5Sq8UGKv/FjEwagA==; Authorization=WEB9d51f674194fab1dc41d417746ae2f7fe28e6890d5c15dd20b6238b1f1a6232f; __zlcmid=1MumtKpLoaG8qnv; kcex_new_guidance=true'
 const authorization = 'WEB348596e998cc0ab8264a2d4f100f113054f85f6a5a32a9fc3ea2d75a75394769'
-orderFuture.futureOrder(body_limit_open, cookie, authorization)
+orderFuture.futureOrder(body_market_open, cookie, authorization)
